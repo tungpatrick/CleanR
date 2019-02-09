b1f4741a94e4894d9b9be5d712c9a322d6e9a396
Patrick Tung <tung_patrick@yahoo.com>
2019-02-08 23:29:14 -0800

    updated R tests

--- tests/testthat/test_locate_na.R
+++ tests/testthat/test_locate_na.R
@@ -20,24 +20,17 @@ test_that("Test that input data is a tbl or data.frame", {
   expect_error(locate_na(TRUE), "Input is not a tibble or data.frame")
 })
 
-# test_that("Test that input data is a tbl or data.frame", {
-#   expect_error(locate_na("Input Data"), "Input is not a tibble or data.frame")
-#   expect_error(locate_na(c(1:10)), "Input is not a tibble or data.frame")
-#   expect_error(locate_na(list(1:3)), "Input is not a tibble or data.frame")
-#   expect_error(locate_na(TRUE), "Input is not a tibble or data.frame")
-# })
-
 test_that("Test valid output format", {
   expect_is(locate_na(toy_tbl), "list")
   expect_is(locate_na(toy_data_df), "list")
 })
 
-test_that("Test functionality of the function", {
+test_that("Test for correct functionality of the function", {
   toy_result <- list(x=c(1), y=c(2,3), z=c(3))
   all_na_result <- list(x=c(1,2,3), y=c(1,2,3), z=c(1,2,3))
   no_na_result <- list(x=c(0), y=c(0))
 
   expect_equal(locate_na(toy_data_tbl), toy_result)
-  expect_equal(locate_na(toy_all_na), all_na_result)
+  expect_warning(locate_na(toy_all_na), "All values are missing.")
   expect_warning(locate_na(toy_no_na), "No NAs are in the input data.")
 })
