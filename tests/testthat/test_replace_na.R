52e261a68226cf9723723188bee2738147023b69
Phuntsok Tseten <phuntsok04@gmail.com>
2019-02-09 16:30:02 -0800

    added tests for replace na

--- tests/testthat/test_replace_na.R
+++ tests/testthat/test_replace_na.R
@@ -2,20 +2,38 @@
 context("Replace NA function")
 library(CleanR)
 
-#Input a data frame
-
-
-input_df <- data.frame((x = c(NA,NA,NA), y = c(NA,NA,NA), z = c(NA, NA, NA))
+toy_data_tbl <- tribble(
+  ~x, ~y,  ~z,
+  NA, 2,  3.6,
+  "b", NA, 8.5,
+  "c", NA, NA
+)
+toy_data_df <- as.data.frame(toy_data_tbl)
+toy_all_na <- tibble(x = c(NA,NA,NA), y = c(NA,NA,NA), z = c(NA, NA, NA))
+toy_no_na <- tibble(x = c(1,2,3,4), y = c(5,6,7,8))
+toy_same_dim <- tibble(x = c(3,4), y = c(5,6))
 
 test_that("Test that input is a data frame", {
   expect_error(replace_na("Input Data"), "Input is not a data frame")
 })
 
 test_that("Test that input has NA", {
-  expect_error(replace_na("Input Data"), "Do not do anything if there are no NA")
+  all_na_result <- list(x=c(1,2,3), y=c(1,2,3), z=c(1,2,3))
+
+  expect_warning(replace_na(toy_all_na), "don't do anything")
+
 })
 test_that("Test that if the whole input has NA", {
-  expect_error(replace_na("Input Data"), "Throw a warning that the input is not valid")
+  no_na_result <- list(x=c(0), y=c(0))
+
+  expect_warning(replace_na(toy_no_na), "No NAs are in the input data.")
 })
+
+test_that("Test that the input dimension equals the output dimension", {
+  same_dim_result <- list(x=c(0), y=c(0))
+
+  expect_warning(replace_na(toy_same_dim), "The dimensions don't match")
+})
+
 
 
